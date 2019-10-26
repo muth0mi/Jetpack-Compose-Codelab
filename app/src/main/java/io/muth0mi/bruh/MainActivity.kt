a83b0227bc41132853c4f78aa44f49d0b786cb96
Oliver Muthomi <oliver.mwitia@gmail.com>
2019-10-26 17:04:46 +0300

    Set up a reusable custom theme

--- app/src/main/java/io/muth0mi/bruh/MainActivity.kt
+++ app/src/main/java/io/muth0mi/bruh/MainActivity.kt
@@ -37,9 +37,7 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun MyApp(child: @Composable() () -> Unit) {
     CustomTheme {
-        Surface() {
-            child()
-        }
+        child()
     }
 }
 
